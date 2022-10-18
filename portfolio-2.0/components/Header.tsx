0ee3f3768cda78cb5a5fc1b7569c88b7c60f5230
MBusto <mikebusto4@gmail.com>
2022-10-18 14:53:51 -0400

    Feat: Finished working on header using framer-motion

--- portfolio-2.0/components/Header.tsx
+++ portfolio-2.0/components/Header.tsx
@@ -1,25 +1,55 @@
 import React from 'react'
 import {SocialIcon} from "react-social-icons"
+import {motion} from "framer-motion"
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start mt-3 justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-      <div className='flex flex-row items-center'>
+      <motion.div 
+      initial={{
+        x:-500,
+        opacity:0,
+        scale:0.5,
+      }}
+      animate={{
+        x:0,
+        opacity:1,
+        scale:1
+      }}
+      transition={{
+        duration:1.8
+      }}
+      className='flex flex-row items-center'>
         {/* Social Icons */}
         <SocialIcon className='mr-5' url="https://linkedin.com/in/michael-busto/" />
         <SocialIcon className='mr-5' url="https://github.com/MBusto4/" />
         <SocialIcon className='mr-5' url="https://upwork.com/freelancers/~01d2f8210968a5fd80" />
-      </div>
+      </motion.div>
 
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <motion.div 
+       initial={{
+        x:500,
+        opacity:0,
+        scale:0.5,
+      }}
+      animate={{
+        x:0,
+        opacity:1,
+        scale:1
+      }}
+      transition={{
+        duration:1.2
+      }}
+      
+      className='flex flex-row items-center text-gray-300 cursor-pointer'>
         <SocialIcon 
         className='cursor-pointer'
         network="email"
         />
         <p className='uppercase hidden md:inline-flex text-sm text-gray-400 ml-3'>Get In Touch!</p>
-      </div>
+      </motion.div>
     </header>
   )
 } 

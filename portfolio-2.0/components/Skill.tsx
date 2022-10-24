6e9f5c8992b8b738b157b393824b40d0677ee231
MBusto <mikebusto4@gmail.com>
2022-10-24 14:31:15 -0400

    Feat: Built out the projects page with dummy data

--- portfolio-2.0/components/Skill.tsx
+++ portfolio-2.0/components/Skill.tsx
@@ -16,11 +16,11 @@ function Skill({directionLeft}: Props) {
 
         whileInView={{opacity:1,x:0}}
         transition={{duration:1}}
-        className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
         src='https://ionicframework.com/docs/icons/logo-react-icon.png'
 />
 
-<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 rounded-full z-0'>
+<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:w-32 xl:h-32 rounded-full z-0'>
     <div className='flex items-center justify-center h-full'>
         <p className='text-3xl font-bold text-black opacity-100'>100%</p>
     </div>

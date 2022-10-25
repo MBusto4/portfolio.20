5468263c922e4b6585797d500f4acc2134710124
MBusto <mikebusto4@gmail.com>
2022-10-24 20:26:46 -0400

    Feat: Worked on adding the data for the content

--- portfolio-2.0/components/About.tsx
+++ portfolio-2.0/components/About.tsx
@@ -36,8 +36,10 @@ function About({}: Props) {
             className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
     />
     <div className='space-y-10 px-0 md:px-10 '> 
-        <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F96666]'>little</span> background </h4>
-        <p className='text-base'>em Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+    <div className='flex items-center justify-center'>
+        <h4 className='text-4xl font-semibold'>Background </h4>
+    </div>
+        <p className='text-lg'>Why challenge yourself? I believe you need to challenge yourself in order to grow as a person and meet goals you have set out for yourself to get where you want to be in life. That's what brought me to coding. What started out as a challenge to myself, turned into a passion.</p>
         </div>
     </motion.div>
     </>
